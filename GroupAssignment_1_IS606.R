e6f294ddf6c11ec2bf108f5a3a56a463b971f749
jhamski <hamski.2@gmail.com>
2015-02-25 23:07:24 -0500

    basic profit margin calcs

--- GroupAssignment_1_IS606.R
+++ GroupAssignment_1_IS606.R
@@ -1,3 +1,15 @@
 # Group Assignment for IS606
 # Members: Honey Berk, Sonya Hong, Rajagopal Srinivasan, James Hamski
 # See'Collaborative Assignment 1.pdf' for instructions
+
+#setwd("/Users/jim/Documents/Graduate\ School/IS\ 606/GroupAssignment_1_IS606")
+details <- read.csv("details.csv")
+sales <- read.csv("sales.csv")
+
+plot(sales$demand.ham)
+
+profit.margin <- details$price - details$cost
+
+ham.daily.cost <- sales$available.ham * details$cost[1]
+
+plot(ham.daily.cost)
