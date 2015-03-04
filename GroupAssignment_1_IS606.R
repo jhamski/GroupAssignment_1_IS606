2c951a125bf30283e364ea5ba7736b9077626c09
jhamski <hamski.2@gmail.com>
2015-03-03 22:46:34 -0500

    Setup anticipated variables

--- GroupAssignment_1_IS606.R
+++ GroupAssignment_1_IS606.R
@@ -1,16 +1,32 @@
 # Group Assignment for IS606
 # Members: Honey Berk, Sonya Hong, Rajagopal Srinivasan, James Hamski
 # See'Collaborative Assignment 1.pdf' for instructions
+# Your job is to use the data to determine how many sandwiches of each type he should bring each day in order to maximize his expected profits. 
 
-# loading these because they're super useful
-library("dplyr")
-library("tidyr")
 
 details <- read.csv("details.csv", header = TRUE)
 sales <- read.csv("sales.csv", header = TRUE)
 
 profit.margin <- details$price - details$cost
 
-ham.daily.cost <- sales$available.ham * details$cost[1]
+ham.revenue <- sales$demand.ham * 6.5
+turkey.revenue <- sales$demand.turkey * 6.5
+veggie.revenue <- sales$demand.veggie * 5.0
+
+ham.cost <- sales$available.ham * 3.5
+turkey.cost <- sales$available.turkey * 4.0
+veggie.cost <- sales$available.veggie * 2.5
+
+# Ham has the greatest profit margin. Is there a relationship between Ham and the other variables?
+# Maximizing Ham could maximize revenue
+
+
+
+
+  
+
+
+
+
 
 plot(ham.daily.cost)
