c613d1b131c7c7a76e4822361cd61843ad34ff57
jhamski <hamski.2@gmail.com>
2015-03-06 22:36:20 -0500

    Final revenue simulation for ham, beginnings of other sandwich types

--- GroupAssignment_1_IS606.R
+++ GroupAssignment_1_IS606.R
@@ -3,8 +3,6 @@
 # See'Collaborative Assignment 1.pdf' for instructions
 # Your job is to use the data to determine how many sandwiches of each type he should bring each day in order to maximize his expected profits. 
 
-library(ggplot2)
-
 details <- read.csv("details.csv", header = TRUE)
 sales <- read.csv("sales.csv", header = TRUE)
 
@@ -27,6 +25,9 @@ distrveggie <- table(sales$demand.veggie)/length(sales$demand.veggie)
 ham.bal <- sales$demand.ham - sales$available.ham
 
 #profit function
+demand.sim<-sales$demand.ham
+inventory.sim<-rpois(1:130,lambda.inventory)
+profit.func(demand.sim, inventory.sim)
 
 profit.func <- function(demand.sim,inventory.sim){
   profit.sim.func <- ifelse(demand.sim-inventory.sim<=0,demand.sim*3.0-(inventory.sim-demand.sim)*3.5, inventory.sim*3.0)
@@ -34,7 +35,7 @@ profit.func <- function(demand.sim,inventory.sim){
   return(profit.sim.func)
 }
 
-profit.func(sales$demand.ham, sales$available.ham)
+obs.profit<-profit.func(sales$demand.ham, sales$available.ham)
 
 #what hits profit/loss harder - money left of the table because inventory is short or unsold inventory?
 foregone.sales <- ifelse(ham.bal>0, (sales$demand.ham-sales$available.ham)*6.5,0)
@@ -56,16 +57,15 @@ hist(sales$demand.ham,col=rgb(1,0,0,1/4), ylim = 30, add=TRUE)
 #Old inentory strategy - make an arbitraty number of sandwiches per day, trying a few different levels
 
 #write model using observed inventory as input
-lambda.demand<-mean(sales$demand.ham)
 
-sales.model.obs<-function(lambda.demand){
-  demand.sim <- sapply(1:130, function(x) rpois(1,lambda.demand))
+sales.model.obs<-function(){
+  demand.sim <- rpois(1:130,16.17)
   inventory.sim <-sales$available.ham
   model.profit <-profit.func(demand.sim, inventory.sim)
 }
 
-model.simulated.profit <-sapply(1:10000, function(x) sales.model.obs(lambda.demand))
-summary(model.simulated.profit)
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.obs())
+#summary(model.simulated.profit)
 difference <- obs.profit - mean(model.simulated.profit)
 difference
 
@@ -73,31 +73,11 @@ difference
 #Example: James runs the following function every morning and makes that many ham sandwiches
 inventory.sim <- sapply(1, function(x) rpois(1,9))
 inventory.sim
-# Model using Poisson to determine number of sandwiches
-sales.model<-function(lambda.inventory){
-  demand.sim <- sapply(1:130, function(x) rpois(1,lambda.demand))
-  inventory.sim <-sapply(1:130, function(x) rpois(1,lambda.inventory))
-  model.profit <-profit.func(demand.sim, inventory.sim)
-}
 
-lambda.inventory = 15
-model.simulated.profit <-sapply(1:10000, function(x) sales.model(lambda.inventory))
-summary(model.simulated.profit)
-difference <- obs.profit - mean(model.simulated.profit)
-difference
-
-#Note that the observed profit is $328 higher, this scenario doesn't work. Could we try differnet lambda values for the inventory?
-lambda.inventory = 16
-model.simulated.profit <-sapply(1:10000, function(x) sales.model(lambda.inventory))
-summary(model.simulated.profit)
-difference <-  mean(model.simulated.profit) - obs.profit
-difference
-
-
-#Inventory Strategy 2 - make between 14 and 18 sandwiches 
+#Inventory Strategy 2 - make a random number of sandwiches between 14 and 16 sandwiches 
 sales.model.2<-function(){
-  demand.sim <- sapply(1:130, function(x) rpois(1,lambda.demand))
-  inventory.sim <-sapply(1:130, function(x) sample(14:18, 1, replace=TRUE))
+  demand.sim <- rpois(1:130,16.17)
+  inventory.sim <-sample(14:17, 130, replace=TRUE)
   model.profit <-profit.func(demand.sim, inventory.sim)
 }
 
@@ -107,27 +87,73 @@ difference <-  mean(model.simulated.profit) - obs.profit
 difference
 
 #Inventory Strategy 3 - make a contant number of sandwiches, say 15 
-sand.no <- 15
-sales.model.3<-function(){
-  demand.sim <- sapply(1:130, function(x) rpois(1,15))
+
+sales.model.3<-function(sand.no){
+  demand.sim <- rpois(1:130, 16.17)
   inventory.sim <-replicate(130, sand.no)
   model.profit <-profit.func(demand.sim, inventory.sim)
 }
-
-model.simulated.profit <-sapply(1:10000, function(x) sales.model.3())
+sand.no <- 14
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.3(sand.no))
 summary(model.simulated.profit)
-difference <-  mean(model.simulated.profit) - obs.profit
-difference
+difference.14 <-  mean(model.simulated.profit) - obs.profit
 
-#17 - 542.5
+sand.no <- 15
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.3(sand.no))
+summary(model.simulated.profit)
+difference.15 <-  mean(model.simulated.profit) - obs.profit
+
+sand.no <- 16
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.3(sand.no))
+summary(model.simulated.profit)
+difference.16 <-  mean(model.simulated.profit) - obs.profit
+
+sand.no <- 17
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.3(sand.no))
+summary(model.simulated.profit)
+difference.17 <-  mean(model.simulated.profit) - obs.profit
+
+constant.count <-cbind(difference.14,difference.15,difference.16,difference.17)
 
 
 ######### TURKEY ######### 
 
-
-
 lambda.turkey <- mean(sales$demand.turkey)
+lambda.turkey
+
+obs.profit.turkey<-profit.func(sales$demand.turkey, sales$available.turkey)
+
+sales.model.obs<-function(){
+  demand.sim <- rpois(1:130,21.6)
+  inventory.sim <-sales$available.turkey
+  model.profit <-profit.func(demand.sim, inventory.sim)
+}
+
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.obs())
+#summary(model.simulated.profit)
+difference <- obs.profit.turkey - mean(model.simulated.profit)
+difference
+
+
+######### VEGGIE ######### 
+
 lambda.veggie <- mean(sales$demand.veggie)
+
+
+##### SCRAPYARD ########
+# Model using Poisson to determine number of sandwiches
+
+sales.model.4<-function(lambda.inventory){
+  demand.sim <- rpois(1:130,16.17)
+  inventory.sim <-rpois(1:130,lambda.inventory)
+  model.profit <-profit.func(demand.sim, inventory.sim)
+}
+
+lambda.inventory = 15
+model.simulated.profit <-sapply(1:10000, function(x) sales.model.4(lambda.inventory))
+summary(model.simulated.profit)
+difference <- mean(model.simulated.profit) - obs.profit
+difference
 
 
 
